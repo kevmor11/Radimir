ba9ff14500447c87799b1dab8610c6b641b7ab9b
kevmor11 <morissette.kevin@gmail.com>
2017-07-19 23:05:01 -0700

    WIP authentication

--- app.js
+++ app.js
@@ -4,36 +4,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var auth = require('basic-auth-connect');
+var cookieSession = require("cookie-session");
 
-var mysql = require('mysql')
+var app = express();
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'dangerous',
+  password : 'root',
   database : 'radimir'
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log('You are now connected...');
-});
+// connection.query('SELECT username, password FROM admins WHERE id = 1', function(err, rows, fields) {
+//     if (err) throw err;
 
-connection.query('SELECT 1 + 2 AS solution', function (err, rows, fields) {
-  if (err) {
-    throw err;
-  }
-
-  console.log('The solution is: ', rows[0].solution)
-});
-
-connection.end();
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-var upload = require('./routes/upload');
-
-var app = express();
-// client.connect();
+//     var username = rows[0].username;
+//     var password = rows[0].password;
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -46,11 +35,61 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['user_id'],
+  // Cookie Options (session cookies expire after 24 hours)
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
-app.use('/upload', upload);
+app.get("/login", (req, res) => {
+  if (req.session.user_id) {
+    res.redirect("/upload");
+  } else {
+    res.render("login", {user: req.session["user_id"]})
+  }
+});
+
+app.post("/login", (req, res) => {
+
+  var queryUsername = '';
+  var queryPassword = '';
+
+  connection.query('SELECT username, password FROM admins WHERE id = 1', function(err, rows, fields) {
+    if (err) throw err;
+
+    queryUsername = rows[0].username;
+    queryPassword = rows[0].password;
+  });
+
+  function authenticated(username, password) {
+    if (queryUsername === username && bcrypt.compareSync(password, queryPassword, 10)) {
+      return;
+    // Break up the if statement into multiple if statements to be able to respond with relevant messages depending on if the email or password does not match
+    }
+  }
+
+  const authenticatedUser = authenticated(req.body.username, req.body.password)
+
+  if (authenticatedUser) {
+    req.session["user_id"] = (authenticatedUser.id)
+    res.redirect("/upload");
+  } else {
+    res.status(403).send("Woops, try again.<br><a href='/register'>Sign Up</a><br><a href='/login'>Login</a>")
+  }
+});
+
+app.get('/upload', function(req, res, next) {
+  if (req.session.user_id) {
+    res.render('upload');
+  } else {
+    res.redirect('/upload');
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,10 +97,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// app.get('/upload', function(req, res) {
-//     res.render('./views/upload.ejs');
-// });
 
 // error handler
 app.use(function(err, req, res, next) {
