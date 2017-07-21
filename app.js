797dff211195d68120fc9de73bba6b7c78625dc8
kevmor11 <morissette.kevin@gmail.com>
2017-07-20 22:41:51 -0700

    album creation completed

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var auth = require('basic-auth-connect');
 var cookieSession = require("cookie-session");
-var gm = require('gm').subClass({imageMagick: true});
+var gm = require('gm').subClass({
+  imageMagick: true
+});
 var bcrypt = require('bcrypt');
 
 var app = express();
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'radimir'
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'radimir'
 });
 
 // view engine setup
@@ -28,7 +30,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieSession({
@@ -38,15 +42,19 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 app.get("/login", (req, res) => {
   if (req.session.user_id) {
     res.redirect("/upload");
   } else {
-    res.render("login", {user: req.session["user_id"]})
+    res.render("login", {
+      user: req.session["user_id"]
+    })
   }
 });
 
@@ -55,7 +63,7 @@ app.post("/login", (req, res) => {
   var queryUsername = '';
   var queryPassword = '';
 
-  var query = connection.query('SELECT username, password FROM admins WHERE id = 1', function(err, rows, fields) {
+  var query = connection.query('SELECT username, password FROM admins WHERE id = 1', function (err, rows, fields) {
     if (err) throw err;
 
     queryUsername = rows[0].username;
@@ -64,7 +72,7 @@ app.post("/login", (req, res) => {
     function authenticated(username, password) {
       if (queryUsername === username && bcrypt.compareSync(password, queryPassword, 10)) {
         return true;
-      // Break up the if statement into multiple if statements to be able to respond with relevant messages depending on if the email or password does not match
+        // Break up the if statement into multiple if statements to be able to respond with relevant messages depending on if the email or password does not match
       }
     }
 
@@ -84,9 +92,9 @@ app.post("/login", (req, res) => {
 
 });
 
-app.get('/upload', function(req, res, next) {
+app.get('/upload', function (req, res, next) {
   // if (req.session.user_id) {
-    res.render('upload');
+  res.render('upload');
   // } else {
   //   res.redirect('/upload');
   // }
@@ -94,39 +102,50 @@ app.get('/upload', function(req, res, next) {
 
 // found this code and the app.get below at https://stackoverflow.com/questions/15772394/how-to-upload-display-and-save-images-using-node-js-and-express
 app.post('/upload', function (req, res) {
-    var tempPath = req.files.file.path,
-        targetPath = path.resolve('./uploads/image.png');
-    if (path.extname(req.files.file.name).toLowerCase() === '.png') {
-        fs.rename(tempPath, targetPath, function(err) {
-            if (err) throw err;
-            console.log("Upload completed!");
-        });
-    } else {
-        fs.unlink(tempPath, function () {
-            if (err) throw err;
-            console.error("Only .png files are allowed!");
-        });
-    }
-    // ...
+  var tempPath = req.files.file.path,
+    targetPath = path.resolve('./uploads/image.png');
+  if (path.extname(req.files.file.name).toLowerCase() === '.png') {
+    fs.rename(tempPath, targetPath, function (err) {
+      if (err) throw err;
+      console.log("Upload completed!");
+    });
+  } else {
+    fs.unlink(tempPath, function () {
+      if (err) throw err;
+      console.error("Only .png files are allowed!");
+    });
+  }
+  // ...
 });
 
 // app.get('/image.png', function (req, res) {
 //     res.sendfile(path.resolve('./uploads/image.png'));
 // });
 
-app.get('/new-album', function(req, res, next) {
+app.get('/new-album', function (req, res, next) {
   res.render('new-album');
 });
 
+app.post('/new-album', function (req, res) {
+  const title = req.body.title;
+  const description = req.body.description;
+    connection.query(`INSERT INTO albums (title, description) VALUES (${mysql.escape(title)}, ${mysql.escape(description)})`,
+      function (err, result) {
+        if (err) throw err;
+        res.redirect('/new-album');
+      }
+    );
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
